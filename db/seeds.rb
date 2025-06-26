60b00a486aa20fa4e11a133509dcef5dc1653e9f
Rafael Coorey <rafi.coorey@gmail.com>
2025-06-26 20:44:47 +1000

    seed file created

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,59 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+puts "Cleaning Database"
+
+Chore.destroy_all
+User.destroy_all
+Household.destroy_all
+
+puts 'Database Clean!'
+puts 'Generating Household'
+
+household = Household.create!(name: "Happy Home")
+
+puts "Household Created!"
+puts "Generating Users!"
+
+User.create!(
+  name: Faker::Name.name,
+  email: "test1@test.com",
+  password: 'password',
+  household: household
+)
+
+User.create!(
+  name: Faker::Name.name,
+  email: "test2@test.com",
+  password: 'password',
+  household: household
+)
+
+User.create!(
+  name: Faker::Name.name,
+  email: "test3@test.com",
+  password: 'password',
+  household: household
+)
+
+puts "Users Created!"
+puts "Generating Chores!"
+
+5.times do
+  completed = [true, false].sample
+  Chore.create!(
+    name: Faker::House.furniture,
+    description: Faker::Lorem.sentence,
+    assigned: [true, false].sample,
+    completed: completed,
+    date_created: Faker::Date.backward(days: 10),
+    date_to_be_completed: Faker::Date.forward(days: 5),
+    completion_date: completed ? Faker::Date.forward(days: 5) : nil,
+    user: household.users.sample,
+    household: household
+  )
+end
+
+puts "Database Seeded!"
