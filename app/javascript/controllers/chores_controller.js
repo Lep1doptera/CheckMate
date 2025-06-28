48b7137583d0760d5a948a148fdc4c21b1efb2c0
Maksym Kozin <maksymkozintest@gmail.com>
2025-06-28 17:04:40 +1000

    drop down working to dsiplay description

--- app/javascript/controllers/chores_controller.js
+++ app/javascript/controllers/chores_controller.js
@@ -1,7 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 
-
 export default class extends Controller {
-  connect() { console.log("hello")
+  static targets = ["description"]
+
+  moreInfo(event) {
+    const wrapper = event.currentTarget.closest("[data-controller='chores']")
+    const choreCard = event.currentTarget.closest("[data-chores-target='chores']")
+    const description = choreCard.querySelector("[data-chores-target='description']")
+    if (description) {
+      description.classList.toggle("hidden")
+    }
   }
 }
