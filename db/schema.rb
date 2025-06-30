f82653ee0da6b8327e3a9834187c594b0e843097
Amanda Ferguson <ferguson.am19@gmail.com>
2025-06-30 22:12:58 +1000

    Installed Invitation for devise and implementing - not working yet

--- db/schema.rb
+++ db/schema.rb
@@ -75,8 +75,19 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_03_091805) do
     t.string "name"
     t.bigint "household_id"
     t.string "default_avatar"
+    t.string "invitation_token"
+    t.datetime "invitation_created_at"
+    t.datetime "invitation_sent_at"
+    t.datetime "invitation_accepted_at"
+    t.integer "invitation_limit"
+    t.string "invited_by_type"
+    t.bigint "invited_by_id"
+    t.integer "invitations_count", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["household_id"], name: "index_users_on_household_id"
+    t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
+    t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
+    t.index ["invited_by_type", "invited_by_id"], name: "index_users_on_invited_by"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
