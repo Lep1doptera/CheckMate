f6f45c439734b6042b1cf8ea240adb08a48fb08f
Rafael Coorey <rafi.coorey@gmail.com>
2025-07-03 20:35:25 +1000

    avatars created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:default_avatar,:profile_picture])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:default_avatar, :profile_picture])
+  end
 end
