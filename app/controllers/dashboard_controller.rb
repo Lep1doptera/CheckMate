eed701607f31846d0bf0f385a8d9bfc3704db538
Maksym Kozin <maksymkozintest@gmail.com>
2025-07-15 19:36:41 +1000

    made the selected week for my purchases

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -12,17 +12,24 @@ class DashboardController < ApplicationController
   end
 
   def my_dashboard
-    @user = current_user
-    @household = @user.household
-    @costings = current_user.costings.order(date: :desc)
-    if @household
-      @chores = @household.chores.where(user_id: @user.id)
-    else
-      @chores = []
+      @user = current_user
+      @household = @user.household
+      all_costings = current_user.costings.order(date: :desc)
+
+      selected_date = params[:week].present? ? Date.parse(params[:week]) : Date.current
+      @start_of_week = selected_date.beginning_of_week(:monday)
+      @end_of_week = selected_date.end_of_week(:monday)
+
+      @costings = all_costings.select { |c| c.date && c.date >= @start_of_week && c.date <= @end_of_week }
+      @weekly_total = @costings.sum(&:amount)
+
+      @weekly_costings = @costings.select { |c| c.date && c.date >= @start_of_week && c.date <= @end_of_week }
+      @weekly_total = @weekly_costings.sum(&:amount)
+
+      @chores = @household ? @household.chores.where(user_id: @user.id) : []
+      @chore_status_data = [
+        ['Complete', @chores.where(completed: true).count],
+        ['Incomplete', @chores.where(completed: false).count]
+      ]
     end
-    @chore_status_data = [
-      ['Complete', @chores.where(completed: true).count],
-      ['Incomplete', @chores.where(completed: false).count]
-    ]
-  end
 end
